d074d47515750200253e16b57f4408b53f3249b6
tux1c0 <37734865+tux1c0@users.noreply.github.com>
2020-02-08 16:13:24 +0100

    Update router.class.php

--- core/class/router.class.php
+++ core/class/router.class.php
@@ -71,7 +71,7 @@ class Router {
 	*/
 	public function setHttpSession() {
 		$this->client = new GuzzleHttp\Client(['base_uri' => $this->getAddress(), 'timeout' => 5.0]);
-		$this->setToken(getToken());
+		$this->setToken($this->getToken());
 	}
 	
 	// get the info
