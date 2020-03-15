5cd5d5247b0c3e4999865708a96447e86f969c1b
tux1c0 <37734865+tux1c0@users.noreply.github.com>
2020-03-15 19:37:45 +0100

    Update sender.py

--- resources/scripts/sender.py
+++ resources/scripts/sender.py
@@ -16,7 +16,7 @@ if len(sys.argv) == 6:
 	connection = AuthorizedConnection('http://'+login+':'+pwd+'@'+ip)
 	client = Client(connection)
 
-	print(json.dumps(client.send_sms(phone, message)))
+	print(json.dumps(client.Sms.send_sms(phone, message)))
 else:
     print("No parameter has been included")
 
