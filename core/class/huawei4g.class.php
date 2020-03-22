c15b0f9a3adf8eddf4ba077659305f097262a7ac
tux1c0 <37734865+tux1c0@users.noreply.github.com>
2020-03-22 12:15:07 +0100

    Update huawei4g.class.php

--- core/class/huawei4g.class.php
+++ core/class/huawei4g.class.php
@@ -35,7 +35,7 @@ class huawei4g extends eqLogic {
 	public static function dependancy_info() {
 		$return = array();
 		$return['progress_file'] = jeedom::getTmpFolder('huawei4g') . '/dependance';
-		if (exec(system::getCmdSudo() . ' -E "python3\-huawei\-lte\-api" | wc -l') >= 1) {
+		if (exec(system::getCmdSudo() . system::get('cmd_check') . ' -E "python3\-huawei\-lte\-api" | wc -l') >= 1) {
 			$return['state'] = 'ok';
 		} else {
 			$return['state'] = 'nok';
