65f5acc0a99fc0a6ddd17c6f4885766450a6ae80
tux1c0 <37734865+tux1c0@users.noreply.github.com>
2020-03-26 12:04:16 +0100

    Update router.class.php

--- core/class/router.class.php
+++ core/class/router.class.php
@@ -134,7 +134,7 @@ class Router {
 	// SMS
 	private function setSMSPython($tel, $msg) {
 		$escapedArg = "'".str_replace("'", "'\\''", $msg)."'";
-		$command = dirname(__FILE__) . '/../../resources/scripts/sender.py '.$this->getIP().' '.$this->getLogin().' '.$this->getPassword().' '.$tel.' '.utf8_encode($escapedArg);
+		$command = dirname(__FILE__) . '/../../resources/scripts/sender.py '.$this->getIP().' '.$this->getLogin().' '.$this->getPassword().' '.$tel.' '.$escapedArg;
 		try{
 			$json = shell_exec('python3 '.$command);
 		} catch (Exception $e){
